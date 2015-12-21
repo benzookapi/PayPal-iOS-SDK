28019f9396ba7e561fd0ff2edbf749287879e913
benzookapi <jokksk@gmail.com>
2015-12-21 12:35:04 +0900

    #

--- SampleApp-Swift/PayPal-iOS-SDK-Sample-App/ZZAppDelegate.m
+++ SampleApp-Swift/PayPal-iOS-SDK-Sample-App/ZZAppDelegate.m
@@ -15,7 +15,7 @@
 
 //#warning "Enter your credentials"
   [PayPalMobile initializeWithClientIdsForEnvironments:@{PayPalEnvironmentProduction : @"AURnLmTIrSf7B8cCdOnj7epoFK2gM2zosJgTeILZQz4_HXy-F8e3Zb-Ysws9Rl5ysL82jUxzXD24iCFr",
-                                                         PayPalEnvironmentSandbox : @"AZE755WCwmQata01pA9JCtpBgjNsqvUg8UOQ3LEghuJVFFGGP3auKYYPYkWKXBOSu5_ZbKzyYII9wGYV"}];
+                                                         PayPalEnvironmentSandbox : @"Acz3G7kG9afqvZF7PJNw-hZvw_F9INQPnZGnSVbclnydHjpK53GxFkW67Wf76FbR8ZzbW_RB3Rznnckq"}];
   return YES;
 }
 
